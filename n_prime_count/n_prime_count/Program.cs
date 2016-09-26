929558f823c52e020fc732451ed724274b080008
SivilTaram <qianlxc@outlook.com>
2016-09-26 20:27:07 +0800

    Initialize empty project.

---
+++ n_prime_count/n_prime_count/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_prime_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
